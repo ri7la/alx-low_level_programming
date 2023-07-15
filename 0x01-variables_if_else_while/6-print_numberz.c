c288b7b81f232016a1be4d2876e3fccebb4da29d
ri7la <benihssane94@gmail.com>
2023-07-14 18:48:13 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - Entry point
+ *Return: alwayse
+ */
+int main(void)
+{
+int n;
+for (n = 0; n <= 9; n++)
+{
+putchar(n + 48);
+}
+putchar('\n');
+return (0);
+}
