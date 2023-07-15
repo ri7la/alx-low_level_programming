f8e5808f331be06da99f0cae8e71239ac4728953
ri7la <benihssane94@gmail.com>
2023-07-14 18:53:45 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - Entry point
+ *Return: alwayse 0
+ */
+int main(void)
+{
+int n = 122;
+while (n >= 97)
+{
+putchar(n);
+n--;
+}
+putchar('\n');
+return (0);
+}
